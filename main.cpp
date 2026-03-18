c49beca8f6f890adf9b1856c31002432d2338d3d
Yanni Guermas <yanniguermas@Mac.attlocal.net>
2026-03-17 21:30:18 -0700

    week 8 lab complete

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include "LinkedQueue.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int main() {
+    LinkedQueue<int> queueA;
+    const int MAX_SIZE = 10;
+
+    try {
+        cout << queueA.front() << " ";
+    } catch (string& e) {
+        cout << e << " ";
+    }
+    try {
+        cout << queueA.back() << " ";
+    } catch (string& e) {
+        cout << e << " ";
+    }
+
+    queueA.enqueue(-5);
+
+    cout << "A length: " << queueA.getLength() << " ";
+    cout << "A front: "  << queueA.front()     << " ";
+    cout << "A back: "   << queueA.back()      << " ";
+
+    queueA.dequeue();
+
+    cout << "A length: " << queueA.getLength() << " ";
+    try {
+        cout << queueA.front() << " ";
+    } catch (string& e) {
+        cout << e << " ";
+    }
+    try {
+        cout << queueA.back() << " ";
+    } catch (string& e) {
+        cout << e << " ";
+    }
+
+    srand(time(0));
+    for (int i = 0; i < MAX_SIZE; i++) {
+        queueA.enqueue(rand() % 500);
+    }
+
+    cout << "A length: " << queueA.getLength() << " ";
+    cout << "A front: "  << queueA.front()     << " ";
+    cout << "A back: "   << queueA.back()      << " ";
+
+    LinkedQueue<int> queueB = queueA;
+
+    cout << "A contains: ";
+    for (int i = 0; i < MAX_SIZE; i++) {
+        cout << queueA.front() << ' ';
+        queueA.dequeue();
+    }
+    cout << " ";
+
+    if (queueA.isEmpty()) {
+        cout << "A is now empty ";
+    } else {
+        cout << "A is not empty [ERROR] ";
+    }
+
+    cout << "B length: " << queueB.getLength() << " ";
+    cout << "B front: "  << queueB.front()     << " ";
+    cout << "B back: "   << queueB.back()      << " ";
+
+    LinkedQueue<int> queueC;
+    queueC = queueB;
+
+    queueB.clear();
+
+    if (queueB.isEmpty()) {
+        cout << "B is now empty ";
+    } else {
+        cout << "B is not empty [ERROR] ";
+    }
+
+    cout << "C length: " << queueC.getLength() << " ";
+    cout << "C front: "  << queueC.front()     << " ";
+    cout << "C back: "   << queueC.back()      << " ";
+
+    return 0;
+}
